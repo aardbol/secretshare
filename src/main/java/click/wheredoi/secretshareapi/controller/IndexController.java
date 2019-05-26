437a1abe279fbbf7d3d92350a4b30faa77662cf6
Leonardo <14614620+aardbol@users.noreply.github.com>
2019-05-26 19:00:03 +0200

    allow cors. fix #4

--- src/main/java/click/wheredoi/secretshareapi/controller/IndexController.java
+++ src/main/java/click/wheredoi/secretshareapi/controller/IndexController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 public class IndexController {
     private SecretService secretService;
