c8cde9bbeef448475ba07885df49fb494104ec0e
Leonardo <14614620+aardbol@users.noreply.github.com>
2019-06-01 10:23:47 +0200

    ignore sensitive production information

--- src/main/java/click/wheredoi/secretshareapi/ScheduledTasks.java
+++ src/main/java/click/wheredoi/secretshareapi/ScheduledTasks.java
@@ -10,10 +10,12 @@ package click.wheredoi.secretshareapi;
 import click.wheredoi.secretshareapi.service.SecretService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("production")
 public class ScheduledTasks {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
